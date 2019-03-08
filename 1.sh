e3a6ca7447e69e7234aa532dda7f9cb126122383
MEiZU-M5 <48317507+MEiZU-M5@users.noreply.github.com>
2019-03-08 13:55:21 +0700

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,228 @@
+clear
+bi='\033[34;1m' #BIRU
+i='\033[32;1m' #IJO
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+me='\033[31;1m' #MERAH
+pu='\033[37;1m' #PUTIH
+ku='\033[33;1m' #KUNING
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"   
+sleep 1
+echo '''\a
+\033[34;1m               _
+\033[34;1m              / |
+\033[34;1m              | |
+\033[34;1m              | |
+\033[34;1m             _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[32;1m              ____
+\033[32;1m             |___ \
+\033[32;1m               __) |
+\033[32;1m              / __/
+\033[32;1m             |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[35;1m             _____
+\033[35;1m            |___ /
+\033[35;1m              |_ \
+\033[35;1m             ___) |
+\033[35;1m            |____/ '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[33;1m             _  _
+\033[33;1m            | || |
+\033[33;1m            | || |_
+\033[33;1m            |__   _|
+\033[33;1m               |_| '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[36;1m              ____
+\033[36;1m             | ___|
+\033[36;1m             |___ \
+\033[36;1m              ___) |
+\033[36;1m             |____/ '''
+sleep 1
+clear
+echo
+echo $cy"==================="$cy" ==================="  
+echo $ku"█"$pu"    Berdoa dulu sebelum menggunakan"$ku"  █"
+echo $ku"█"$pu"====================================="$ku"█"
+echo $ku"█"$pu"  Author  :  Edi           "$ku"          █"
+echo $ku"█"$pu"  WhatsApp:  083808341xxx"$ku"            █"
+echo $ku"█"$pu"  Gmail   :  edi.nasa03@gmail.com"$ku"    █"
+echo $ku"█"$pu"  Youtube :  Edi Garsell"$ku"             █"
+echo $ku"█"$pu"  Github  :  github.com/MEiZU-M5"$ku"     █"
+echo $cy"==================="$cy" ==================="
+echo
+echo $i"->Menu:"
+echo $ku"{"$me"1"$ku"}"$cy" Report FB"
+echo $ku"{"$me"2"$ku"}"$cy" Brute Force"
+echo $ku"{"$me"3"$ku"}"$cy" Brute Force Facebook Cracker"
+echo $ku"{"$me"4"$ku"}"$cy" Mbf"
+echo $ku"{"$me"5"$ku"}"$cy" Cloning Yahoo"
+echo $ku"{"$me"6"$ku"}"$cy" Bot Reaction Facebook"
+echo $ku"{"$me"7"$ku"}"$cy" Spam Call"
+echo $ku"{"$me"8"$ku"}"$cy" Youtube-dl"
+echo $ku"{"$me"9"$ku"}"$cy" Keluar"
+echo
+echo $ku"╭─{"$i"Pilihlah sesuai ke inginan Anda"$ku"}"
+read -p"╰─> : " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/3.git
+cd 3
+python2 MEiZU-M5.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/7.git
+cd 7
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/1.git
+cd 1
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/2.git
+cd 2
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/5.git
+cd 5
+python2 blackbot.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/4.git
+cd 4
+php SpamCall.php
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/6.git
+cd 6
+sh youtube-dl.sh
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "E X I T"|Ulolcat
+sleep 2
+echo $i"SEMANGAT PAGI..."
+echo $i"Terima Kasih Akang sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan Akang di pakai semoga bermanfaat"
+sleep 2
+echo $i"Buat Akang²"
+sleep 2
+echo $i"Bila Akang belum mengerti Akang Bisa Nanya"
+echo $i"Melalui Via:"
+sleep 2
+echo $ku"Facebook :"$pur" Edi"
+echo $ku"YouTube  :"$pur" Edi Garsell"
+echo $ku"WhatsApp :"$pur" 083808341xxx"
+sleep 2
+echo $i"dan jangan lupa "$cy" LIKE SHARE AND SUBSCRIBE"
+sleep 2
+echo $i"Video Channel "$cy" Edi Garsell"
+sleep 2
+echo $i"Agar tambah semangat Upload Video²"
+sleep 2
+echo $i"terbaru menarik dari saya"
+echo $i"silahkan Aktifkan juga ya Akang Tombol Loncengnya"
+sleep 2
+echo $i"Agar dapat pemberitahuan Video²"
+echo $i"selanjutnya dari saya OKE"
+sleep 2
+echo
+echo $cy"Thanks FOR WATCHING....!!!"
+exit
+fi
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# edi
+
+## MAU NGAPAIN INTIP" TOOLS INI GA ADA KERJAAN 
+## SEBELUM MENGGUNAKAN TOOLS INI
+## SILAHKAN SUBCRIBE DULU MY CHANNEL ( Edi Garsell )
+
+# Note :
+### $ jangan Di gunakan 
+### $ Untuk Hal jahat ya atau berniat Semena mena
+### $ Mari kita Bangun Chanel (Edi Garsell)
+### $ Bersama karena kebersamaan adalah kunci dari kesuksesan
+
+# SILAHKAN FOLLOW DAN SUBSCRIBE YA:
+### Pollow 
+### FACEBOOK : https://www.facebook.com/Windows7.htmlx1
+### Subcribe 
+### YOUTUBE  : https://www.youtube.com/channel/UCr1X7uS9_FEjITKDah2FxXw/videos?view_as=subscriber
+# ™
+### Anda sopan saya siap melayani Anda
+### jika anda tidak sopan ya udah Tinggalin Saja 
+# SEKIAN DAN TERIMA KASIH.......!!!
