b3998e7edb586824d78820da323e5706d062dd46
MEiZU-M5 <48317507+MEiZU-M5@users.noreply.github.com>
2019-03-09 05:26:56 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -87,7 +87,7 @@ echo
 echo $pur"~~~~~~~~~~~~~~~~~~~"$pur" ~~~~~~~~~~~~~~~~~~~"  
 echo $cy"█"$i"   --» SELAMAT DATANG BOS «--"$cy"         █"
 echo $cy"█"$me"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"$cy" █"
-echo $cy"█"$i" Author    :  Edi            "$cy"         █"
+echo $cy"█"$i"  AUTHOR  :  EDI           "$cy"          █"
 echo $cy"█"$i" WhatsApp  :  083808341xxx"$cy"            █"
 echo $cy"█"$i" Gmail     :  edi.nasa03@gmail.com"$cy"    █"
 echo $cy"█"$i" Youtube   :  Edi Garsell"$cy"             █"
